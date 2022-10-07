cffab0e4005813f0873b3ec96d5099caf166754a
Destakka <d.atikananda.ft21@mail.umy.ac.id>
2022-10-07 17:26:18 +0700

    Deklarasi array,variable,dan fungsi / method

--- act1_bubblesort/Program.cs
+++ act1_bubblesort/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Bubble_Sort
+{
+    class Program
+    {
+        //Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        //Deklarasi variable int untuk menyimpan banyaknya data pada array
+        private int n;
+
+        //Fungsi / Method untuk menerima masukan
+        public void read()
+        {
+            //Menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _");
+            Console.WriteLine(" Masukkan elemen array ");
+            Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _");
+
+            //pengguna memasukkan elemen pada array
+            for (int i = 0, i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
+        }
+    }
+}
